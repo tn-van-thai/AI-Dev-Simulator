e8f3df9e37e4ffc757c83e8d617457a675a15020
GodLYEI <thaituan3801@gmail.com>
2026-02-07 12:21:48 -0600

    fixed conflicts and bugs after merged

--- player.gd
+++ player.gd
@@ -105,3 +105,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		player_in_range = false
+
+func modify_ethic(value : int):
+	ethic_scale += value
+	print(ethic_scale)
